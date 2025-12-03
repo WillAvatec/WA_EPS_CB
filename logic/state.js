6fddab2239e13a916e01e5aeeb837bf31a9a261c
Lizbeth-Laptop <227698309+manuishere2312-spec@users.noreply.github.com>
2025-12-03 17:27:43 -0500

    adds a full object

--- logic/state.js
+++ logic/state.js
@@ -2,12 +2,18 @@ const sessions = {}; // { userId: { stage, category, item, quantity, payment } }
 
 function getSession(userId) {
   if (!sessions[userId]) {
-    sessions[userId] = { stage: "menu" };
+    sessions[userId] = {
+      stage: "menu",
+      payment: "",
+      order: {
+        main:"",
+        starter:""
+      }
+    };
   }
   return sessions[userId];
 }
 
 module.exports = { getSession };
-
 
 // ESTE CODIGO NO ASEGURA TENER DATA PERSISTENCE, se requiere tener una base de datos temporal posiblemente
